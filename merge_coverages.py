8273e3e390844282b54d3905dd226d1140b098b4
v-julia <vjulia94@gmail.com>
2019-09-04 19:53:49 +0300

    Add files via upload

--- merge_coverages.py
+++ merge_coverages.py
@@ -1,11 +1,18 @@
+from time import time
+
+t1 = time()
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 from pathlib import Path
-from time import time
+
 from Bio import SeqIO
+
+t2 = time()
+
+print(t2-t1)
 
 def merges_coverage(input_dir, out_dir, path_alignment, title, first_name):
     '''
@@ -30,79 +37,113 @@ def merges_coverage(input_dir, out_dir, path_alignment, title, first_name):
                                     'gapopen','qstart','qend','sstart','send','evalue','bitscore']) #.transpose()
 
 
-
     # getting the other blast output dataframes with the rows distinct from the first df
     for name in blast_outputs_names:
         if name == first_name:
             continue
         else:
             print(name)
-            blast_out_dict[name] = compare_blast_out(Path(input_dir,first_name),Path(input_dir,name))
+            blast_out_dict[name] = pd.read_csv(Path(input_dir,name), sep='\t', header = None, \
+                                    names=['qseqid','sseqid','pident','length','mismatch',\
+                                    'gapopen','qstart','qend','sstart','send','evalue','bitscore']) #.transpose()
+            #blast_out_dict[name] = compare_blast_out(Path(input_dir,first_name),Path(input_dir,name))
             print(len(blast_out_dict[name]))
     # print(blast_out_dict["blast_AY593810.1.out"])
+
 
     # alignment of reference sequences
     records_temp = SeqIO.to_dict(SeqIO.parse(open(path_alignment), "fasta"))
 
-    # dict_relative[seq_id] = [] list with relative positions of nucleotides in sequences 
-    # of seq_id in the alignment of reference sequences
-    # seq_id - id of reference sequence in records_temp
-    dict_relative = {}
-    # Dictionary with coverages according to the relative positions of sequences in the alignments
-    dict_coverage = {}
+
+
+
+    # list with positions of sequence's nucleotides in alignment 
+    # of reference sequences
+    global rel_pos_list
+
+    # list with ids of all blast hits from all files
+    seq_ids_all = []
+
+    # Adds new columns to each dataframe - 'sstart_al' and 'send_al'
+    # with positions in sseq according to the alignment of all reference sequence
+    for name in blast_outputs_names:
+        '''
+        # sequence blast was run against
+        seq = str(records_temp[name.strip(".out").strip("blast_")].seq)
+        # length of sequence without gaps
+        seq_ng_length = len(seq.replace('-',''))
+
+        rel_pos_list = make_pos_list(seq)
+
+        blast_out_dict[name]['sstart_al'] = blast_out_dict[name].apply(get_sstart_pos_in_al, axis=1)
+        blast_out_dict[name]['send_al'] = blast_out_dict[name].apply(get_send_pos_in_al, axis=1)
+        print(blast_out_dict[name].head())
+        '''
+        # adds all qseqid ids to list
+        [seq_ids_all.append(id) for id in list(blast_out_dict[name]['qseqid'])]
+
+    # unique ids in all files with blast output
+    seq_ids_all = np.unique((np.array(seq_ids_all)))
+    print(len(seq_ids_all))
+
+    # dictionary with length of coverage for sequences from blast output
+    # length_dict[seq_id][blast_out_name] = length
+    # seq_id - qseq, blast_out_name - name of blast table,
+    # length - sum of 'length' columns in table blast_out_name for sequence with id seq_id
+    length_dict = {}
+    t1 = time()
+    for seq_id in seq_ids_all:
+        length_dict[seq_id] = {}
+        for name, df in blast_out_dict.items():
+            length_dict[seq_id][name] = sum(df[df['qseqid'] == seq_id]['length'])
+
+    # dataframe
+    # column names - ids of sequences
+    # indices - names of blast output tabels
+    # values - length of blast hit seq_id in blast output table
+    length_df = pd.DataFrame.from_dict(length_dict)
+    t2 = time()
+    print(t2-t1)
+    print(length_df.head())
+
+    # series where value is the name of blast ouput table where length of vlast hit is maximal
+    # if there are two tables with maximal length the first one is chosen
+    length_df = length_df.apply(lambda col: col[col==max(col)].index[0], axis=0)
+    print(length_df.head())
+    # new table with results from several blast runs
+    # for each sequence has string from the blast table where query length was the highest
+    blast_table_new = length_df.apply(lambda x: blast_out_dict[str(x[0])][blast_out_dict[str(x[0])]==x.name],axis=0)
+    print(blast_table_new.head())
+    # sorted by sseqid
+    blast_table_new = blast_table_new.sort_values(by='sseqid')
+
+    # dictionary with lists of relative positions of ref sequences in the alignment
+    rel_pos_l_dict = {}
+    print(blast_outputs_names)
+    for name in blast_outputs_names:
+        # sequence blast was run against
+        seq_name = name.strip(".out").strip("blast_")
+        seq = str(records_temp[seq_name].seq)
+        rel_pos_l_dict[seq_name] = make_pos_list(seq)
+
+    def get_sstart_pos_in_al(row):
+        r = rel_pos_l_dict[row['sseqid']].index(row['sstart'])
+        return rel_pos_l_dict[row['sseqid']].index(row['sstart'])
+
+    def get_send_pos_in_al(row):
+        r = rel_pos_l_dict[row['sseqid']].index(row['send'])
+        return rel_pos_l_dict[row['sseqid']].index(row['send'])
+    blast_table_new['sstart_al'].apply(get_sstart_pos_in_al, axis=0)
+    blast_table_new['send_al'].apply(get_send_pos_in_al, axis=0)
+
+    print(blast_table_new.head())
+
+
 
     reference_length = len(records_temp[list(records_temp)[0]].seq)
-    final_coverage = [0]*reference_length
-    print("Total length of alignment {}".format(str(reference_length)))
 
-    for proto_id in blast_outputs_names:
-        print(proto_id)
-        # reference sequence with gaps
-        seq = str(records_temp[proto_id.strip(".out").strip("blast_")].seq)
-        seq_ng_length = len(seq.replace('-',''))
-        rel_pos_list = make_pos_list(seq)
-        #print(rel_pos_list)
-        coverage_list = make_cov_list(blast_out_dict[proto_id], seq_ng_length)
-        #print(len(coverage_list))
-        #print(coverage_list)
-        temp_rel_cov = [0]*len(records_temp[list(records_temp)[0]].seq)
-        for i in range(1,seq_ng_length+1):
-            temp_rel_cov[rel_pos_list.index(i)] = coverage_list[i-1]
-        final_coverage = np.add(final_coverage, temp_rel_cov)
+    final_coverage = make_cov_list(blast_out_dict[proto_id], reference_length)
 
-    final_coverage_nogap = []
-    for i in range(len(final_coverage)):
-        if i > 100 and i < 8000:
-            if final_coverage[i] < 200:
-                continue
-            else:
-                final_coverage_nogap.append(final_coverage[i])
-        '''
-        else:
-            if i < 3100 and i > 2900:
-                if final_coverage[i]<200:
-                    continue
-                else:
-                    final_coverage_nogap.append(final_coverage[i])
-            else:
-                    final_coverage_nogap.append(final_coverage[i])
-        '''
-        '''
-        if i > 100 and i < 3100:
-            if final_coverage[i] < 50:
-                continue
-            else:
-                final_coverage_nogap.append(final_coverage[i])
-        else:
-            if i < 3950 and i > 3400:
-                if final_coverage[i]<1300:
-                    continue
-                else:
-                    final_coverage_nogap.append(final_coverage[i])
-            else:
-                    final_coverage_nogap.append(final_coverage[i])
-        '''
-    print(len(final_coverage))
     #plt.hist(range(1,len(final_coverage_nogap)+1,1), bins= range(1,len(final_coverage_nogap)+1,1), weights = final_coverage_nogap)
     plt.hist(range(1,len(final_coverage)+1,1), bins= range(1,len(final_coverage)+1,1), weights = final_coverage)
     plt.xlabel("Position in genome, nt")
@@ -112,6 +153,7 @@ def merges_coverage(input_dir, out_dir, path_alignment, title, first_name):
     plt.savefig(out_dir+title+".svg")
     plt.show()
     plt.clf()
+    '''
     plt.hist(range(1,len(final_coverage_nogap)+1,1), bins= range(1,len(final_coverage_nogap)+1,1), weights = final_coverage_nogap)
     plt.xlabel("Position in genome, nt")
     plt.ylabel("Number of sequences in GenBank")
@@ -119,12 +161,12 @@ def merges_coverage(input_dir, out_dir, path_alignment, title, first_name):
     plt.savefig(out_dir+title+"_nogap.png")
     plt.savefig(out_dir+title+"_nogap.svg")
     #plt.show()
-
+    '''
     final_coverage_s = [str(x) for x in final_coverage]
-    final_coverage_nogap_s = [str(x) for x in final_coverage_nogap]
+    #final_coverage_nogap_s = [str(x) for x in final_coverage_nogap]
     with open(os.path.join(out_dir, title+"_cov.txt"),"w") as out_file:
         out_file.write(','.join(final_coverage_s))
-        out_file.write(','.join(final_coverage_nogap_s))
+        #out_file.write(','.join(final_coverage_nogap_s))
     out_file.close()
 
 def compare_blast_out(blast_output1, blast_output2):
@@ -189,8 +231,6 @@ def make_cov_list(blast_out_df, reference_length):
         blast was run against.
     '''
 
-    #ID of reference seq blast was performed against
-    ref_id = blast_out_df['sseqid'][list(blast_out_df.index)[0]]
 
     # list for sequence counts in each position of reference sequence
     pos_coverage = [0]*reference_length
